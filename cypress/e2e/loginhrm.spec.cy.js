91585a535bc201a84d1fd2fc14448307c86ffa80
Miguel <miguel.manutencao@hotmail.com>
2026-02-04 15:01:48 -0300

    add const selectorsList

--- cypress/e2e/loginhrm.spec.cy.js
+++ cypress/e2e/loginhrm.spec.cy.js
@@ -1,20 +1,28 @@
 import UserData from '../fixtures/UserData.json'
 
 
-
 describe('Orange HRM Tests', () => {
+
+  const selectorsList = {
+  userNameField: "[name='username']",
+  passwordField: '[name="password"]',
+
+  }
+
+
+
   it('Login - Success', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get('[name="username"]').type(UserData.userSuccess.username)
-    cy.get('[name="password"]').type(UserData.userSuccess.password)
+    cy.get(selectorsList.userNameField).type(UserData.userSuccess.username)
+    cy.get(selectorsList.passwordField).type(UserData.userSuccess.password)
     cy.get("[type='submit']").click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
     cy.get('.oxd-topbar-header-breadcrumb-module').contains('Dashboard')
   })
     it.skip('Login - Fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get('[name="username"]').type('Admin')
-    cy.get('[name="password"]').type('falha')//password errado
+    cy.get(selectorsList.userNameField).type('Admin')
+    cy.get(selectorsList.passwordField).type('falha')//password errado
     cy.get("[type='submit']").click()
     cy.get('.oxd-alert')
     
