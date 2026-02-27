6435a3dc2dc537dd4a3e214cfa0d953379128e42
Miguel <miguel.manutencao@hotmail.com>
2026-02-27 18:53:53 -0500

    adicionado .github\workflow e configurado o yml

--- cypress.config.js
+++ cypress.config.js
@@ -7,5 +7,17 @@ module.exports = defineConfig({
     },
 
     baseUrl: 'https://opensource-demo.orangehrmlive.com/web/index.php',
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    retries: {
+      runMode: 2,
+      openMode: 0
+    },
+    video: true,
+    screenshotOnRunFailure: true,
+    fixturesFolder: 'cypress/fixtures',
+    screenshotsFolder: 'cypress/screenshots',
+    videosFolder: 'cypress/videos',
+    supportFile: 'cypress/support/e2e.js'
   },
 });
